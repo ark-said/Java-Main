acff583fc476e4128f9b19f652e9037d2347b4fc
Ali Said Ark <arkalisaid@gmail.com>
2022-05-16 07:34:20 +0300

    v6

---
+++ src/day34_accessModifier_encapsulation/C08_Notes.java
@@ -0,0 +1,33 @@
+package day34_accessModifier_encapsulation;
+
+public class C08_Notes {
+	
+	//private'a sadece olusturuldugu class olusabilir.
+
+	/*methodlar private ve public (default) olabilir.
+	  protected/private olamaz. /*
+ 
+	// Görünen bir atama yoksa default access modifier (public) geçerli olur.
+
+	/* default access modifier=package access modifier
+	 sadece o package'dekiler ulaşabilir. /*
+
+
+	/*	protected - kendi class'ı ulaşır.
+		kendi package'si ulaşır.
+	 	başka package'lerde bile olsa child class'lar oluşur.
+		kim ulaşamaz: başka package'da ve child olmayanlar. */
+
+	// static olmayanlara obje üzerinden ulaşılabilir. aynı class'da iseniz  her şeye obje üzerinden ulaşabiliriz.
+
+
+	//private SADECE ve SADECE kendi class'ındaki kullanılabilir.  (class uyeleri tarafından)
+
+
+	//BIR method gorulebilsin ama degistirilemesin istiyorsan GETTER kullanmak gerekiyor. Source> Getters Setters 
+
+
+	
+	
+
+}
