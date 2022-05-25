5b8c8b0559a8e99d50ce6e633ab65bf0d212db10
Ali Said Ark <arkalisaid@gmail.com>
2022-05-25 22:28:16 +0300

    v16

---
+++ src/day41_errors_garbageCollectors/C01_Exceptions.java
@@ -0,0 +1,22 @@
+package day41_errors_garbageCollectors;
+
+public class C01_Exceptions {
+	
+	final static double pi=3.14;
+
+	public static void main(String[] args) {
+		
+		
+		/*for (int i = 0; i < 10; i--) {
+			i--;
+		}
+		*/
+		// sinirsiz olur data base dolar pc kitlenir.
+		// Error kodlarla ongorulemez uncheck'dirler
+		// handle edilemezler
+		// genelde run time de gorulurler
+		// example : OutOfMemory
+
+	}
+
+}
---
+++ src/day41_errors_garbageCollectors/C02_Final.java
@@ -0,0 +1,17 @@
+package day41_errors_garbageCollectors;
+
+public class C02_Final {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println(C01_Exceptions.pi);
+		
+		// final koyarsak override edilemez
+		// degeri degistiremeyiz
+		// static hem erisimi kolay 
+		// hem de degistirilemez source olarak kullanmak icin basina final koruz
+		
+		
+	}
+}
